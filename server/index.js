50f99f7e8787afa7c387b99e95a044dfc1067c31
Jasoliya Brijesh <jasoliyabrijesh123@gmail.com>
2023-01-16 10:27:19 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -64,6 +64,7 @@ if(isDev) {
     app.use(serveStatic(STATIC_PATH, { index: false }));
 
     app.use("/*", shopify.ensureInstalledOnShop(), async (req, res, next) => {
+        console.log('call');
         res
             .status(200)
             .set("Content-Type", "text/html")

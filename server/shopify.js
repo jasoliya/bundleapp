c7eb79523c8dd573b317ba098d8ee553053542c7
Jasoliya Brijesh <jasoliyabrijesh123@gmail.com>
2023-01-31 14:54:37 +0530

    changes

--- server/shopify.js
+++ server/shopify.js
@@ -2,6 +2,7 @@ import { BillingInterval, LATEST_API_VERSION } from '@shopify/shopify-api';
 import { shopifyApp } from '@shopify/shopify-app-express';
 import { SQLiteSessionStorage } from '@shopify/shopify-app-session-storage-sqlite';
 import { restResources } from '@shopify/shopify-api/rest/admin/2023-01';
+import cookieParser from 'cookie-parser';
 
 import dotenv from 'dotenv';
 dotenv.config();

6497c94c9cbebc46e080756c5ea7e9647e5f0f20
Jasoliya Brijesh <jasoliyabrijesh123@gmail.com>
2023-02-02 11:38:09 +0530

    changes

--- server/middleware/bundle-api.js
+++ server/middleware/bundle-api.js
@@ -420,7 +420,7 @@ export default function bundleApiEndpoints(app) {
         } catch (e) {
             status = 500;
             error = e.message;
-            console.log(e);
+            console.log(e.response);
         }
         
         res.status(200).send({ success: status === 200, data, error });
